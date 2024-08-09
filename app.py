d7f2cb5fbcd3525bc5f23f3d2165e5d45aafacbd
dev.prathamesh <byteninjacode@gmail.com>
2024-08-09 11:41:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -245,7 +245,3 @@ def submit_contact_form():
         })
 
 
-if __name__ == '__main__':
-    from werkzeug.serving import run_simple
-    run_simple('0.0.0.0', 5000, app, use_reloader=False, use_debugger=True)
-
